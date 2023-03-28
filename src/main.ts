2ab4c6bfbd433d4609e29ccd8273f144b912b4ed
Marcin <wanago.marcin@gmail.com>
2023-03-29 01:39:49 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { LogLevel, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { LoggerInterceptor } from './utils/logger.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const isProduction = process.env.NODE_ENV === 'production';
@@ -25,6 +26,15 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.useGlobalInterceptors(new LoggerInterceptor());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API with NestJS')
+    .setDescription('API developed throughout the API with NestJS course')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('PORT'));
 }
